124a225127ba819a90619f34452604df6d65b6b1
Matthew Bendall <bendall@gwu.edu>
2018-01-25 13:24:20 -0500

    gff instead of bed

--- scripts/extract_genes.py
+++ scripts/extract_genes.py
@@ -2,21 +2,24 @@
 from __future__ import print_function
 import sys
 from Bio import SeqIO
+import re
 
 if __name__ == '__main__':
     import argparse
     parser = argparse.ArgumentParser(description='extract genes from fasta.')
     parser.add_argument('fasta', type=argparse.FileType('rU'))
-    parser.add_argument('bed', type=argparse.FileType('rU'))        
+    parser.add_argument('gff', type=argparse.FileType('rU'))        
     parser.add_argument('outfile', nargs='?', type=argparse.FileType('w'), default=sys.stdout)
     args = parser.parse_args()
     
     seqs = {s.id:s for s in SeqIO.parse(args.fasta, 'fasta')}
-    bed = (l.strip('\n').split('\t') for l in args.bed)
-    for rec in bed:
-        s = seqs[rec[0]][int(rec[1]):int(rec[2])]
-        if rec[5] == '-':
+    gff = (l.strip('\n').split('\t') for l in args.gff if not l.startswith('#'))
+    for rec in gff:
+        if rec[2] != 'gene': continue
+        s = seqs[rec[0]][(int(rec[3])-1):int(rec[4])]
+        if rec[6] == '-':
             s = s.reverse_complement()
-        s.id = '%s.%s' % (s.id, rec[3])
-        s.description = '%s-%s(%s)' % (rec[1], rec[2], rec[5])
+        attrs = dict(f.split('=') for f in rec[8].split(';'))
+        s.id = '%s.%s' % (s.id, attrs['Name'])
+        s.description = '%s-%s(%s)' % (rec[3], rec[4], rec[6])
         SeqIO.write(s, args.outfile, 'fasta')
